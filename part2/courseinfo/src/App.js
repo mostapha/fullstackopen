2ec471d3ded703af3faf1c90d46a4b2aff532902
Mustapha Bouh <67738751+mostapha@users.noreply.github.com>
2023-02-18 22:53:24 +0100

    exercise 2.5: separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,41 +1,4 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  )
-}
-
-const Content = (props) => {
-  console.log('Content props', props);
-  const parts = props.parts
-
-  return (
-    <div>
-      {parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)}
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const parts = props.parts
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return <b>total of {total} exercises</b>
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
