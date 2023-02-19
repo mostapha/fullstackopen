e2cf4f89a29a66a690fb28cd728c0ed34c4fb193
Mustapha Bouh <67738751+mostapha@users.noreply.github.com>
2023-02-19 15:10:00 +0100

    exercise 2.10: The Phonebook Step5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,29 @@
 import { useState } from 'react'
 
+
+// components
+const SearchFilter = ({ onSearchChange, newSearch }) => {
+  return <div>filter shown with: <input onChange={onSearchChange} value={newSearch} /></div>
+}
+
+const NamesForm = ({ onFormSubmit, onNameChange, newName, onNumberChange, newNumber }) => {
+  return (
+    <form onSubmit={onFormSubmit}>
+      <div>name: <input onChange={onNameChange} value={newName} /></div>
+      <div>number: <input onChange={onNumberChange} value={newNumber} /></div>
+      <div><button type="submit">add</button></div>
+    </form>
+  )
+}
+
+const Persons = ({ people }) => {
+  return (
+    <ul>
+      {people.map(person => <li key={person.name}>{person.name + " " + person.number}</li>)}
+    </ul>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -7,27 +31,18 @@ const App = () => {
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
-  
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
 
-  const onSearchChange = event => {
-    setNewSearch(event.target.value)
-  }
+  // helpers
+  const isNameAlreadyAdded = name => persons.some(person => person.name === name)
 
-  const onNameChange = event => {
-    setNewName(event.target.value)
-  }
-
-  const onNumberChange = event => {
-    setNewNumber(event.target.value)
-  }
-
-  const isNameAlreadyAdded = name => {
-    return persons.some(person => person.name === name)
-  }
-
+  // events
+  const onSearchChange = event => setNewSearch(event.target.value)
+  const onNameChange = event => setNewName(event.target.value)
+  const onNumberChange = event => setNewNumber(event.target.value)
   const onFormSubmit = event => {
     event.preventDefault();
 
@@ -57,17 +72,17 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
-      <div>filter shown with: <input onChange={onSearchChange} value={newSearch} /></div>
+      <SearchFilter onSearchChange={onSearchChange} newSearch={newSearch} />
       <h2>Add new name</h2>
-      <form onSubmit={onFormSubmit}>
-        <div>name: <input onChange={onNameChange} value={newName} /></div>
-        <div>number: <input onChange={onNumberChange} value={newNumber} /></div>
-        <div><button type="submit">add</button></div>
-      </form>
+      <NamesForm
+        onFormSubmit={onFormSubmit}
+        onNameChange={onNameChange}
+        newName={newName}
+        onNumberChange={onNumberChange}
+        newNumber={newNumber}
+      />
       <h2>Numbers</h2>
-      <ul>
-        {personsToShow.map(person => <li key={person.name}>{person.name + " " + person.number}</li>)}
-      </ul>
+      <Persons people={personsToShow} />
     </div>
   )
 }
